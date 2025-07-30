0055a81b00d8962ccfda5f972dc5f5294664c34e
khopsickle <khopsickle@users.noreply.github.com>
2025-07-30 15:54:02 -0700

    Remove boilerplate

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "User Dashboard - Wave Health" },
+    { name: "User Dashboard", content: "Take home assessment" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <>test</>;
 }
